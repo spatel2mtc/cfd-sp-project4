929be28d4a865a3655e4719ee7994d2589b37790
sp323 <patel.sujal.r@gmail.com>
2021-12-09 22:01:47 -0600

    updating app.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sujal Patel was here but now he's going to Miami!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
